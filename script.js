33b83135a20c0d758ca0aed5d2e80f252b43f25a
felipeyamadaw <felipeyamadaw17@gmail.com>
2025-06-27 16:31:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let num1 = parseInt (prompt ('Me informe o número 1:'));
+let num2 = parseInt (prompt ('Me informe o número 2:'));
+let num3 = parseInt (prompt ('Me informe o número 3:'));
+
+let resultadosoma = num1 + num2 + num3;
+let media = resultadosoma / 3;
+
+console.log (' O resultado da soma é:' + media);
+ 
